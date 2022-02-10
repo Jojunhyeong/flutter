0b2e3a85ae3540d674092179794a4ca2d5a4b612
jojh0323 <jojh0323@naver.com>
2022-02-10 16:21:16 +0900

    1

---
+++ why.dart
@@ -0,0 +1,21 @@
+bool isEven(int x){
+  if(x%2==0){
+    return true;
+  }else {
+    return false;
+  }
+}
+
+List<int> getEvenNumbers(Iterable<int> numbers){
+  var evenNumbers = <int>[];
+  
+  for(var i in numbers) {
+    if (isEven(i)) evenNumbers.add(i);
+  }
+  return evenNumbers;
+}
+
+main(){
+  var numbers = List.generate(10, (i) => i);
+  print(getEvenNumbers(numbers));
+}

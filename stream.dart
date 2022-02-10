25abce4012f6e47c3c0e1b636275368a8f298ff6
jojh0323 <jojh0323@naver.com>
2022-02-10 17:06:33 +0900

    second page_220210

---
+++ stream.dart
@@ -0,0 +1,10 @@
+main() {
+  print('a single quoted string');
+  print("a double quoted string");
+  
+  print("cat" + "dog");
+  print('''triple quoted strings 
+  are for multiple lines''');
+  var pi=3.14;
+  print('pi is $pi');
+  print('tau is ${2*pi}');
